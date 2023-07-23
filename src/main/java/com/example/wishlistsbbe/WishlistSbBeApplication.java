29b634e4668ad8b31be3b80769c21ccb1a5d5486
Rafael Falcão <rafaelfgjardim@gmail.com>
2023-07-23 19:05:32 -0300

    First commit

---
+++ src/main/java/com/example/wishlistsbbe/WishlistSbBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.wishlistsbbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishlistSbBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WishlistSbBeApplication.class, args);
+    }
+
+}
